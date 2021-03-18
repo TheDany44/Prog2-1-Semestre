e4d453bb7dea47d5238734aa95634e8055ae8398
TheDany44 <dani2002lopes@gmail.com>
2021-03-18 23:06:30 +0000

    P00 ate prob5 feito

--- P00/Prob5/p5.c
+++ P00/Prob5/p5.c
@@ -1,11 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+void inverte(char *strOriginal, char *strInvertida){
+    int i,a=0;
+    for(i=strlen(strOriginal)-1;i>=0;i--){
+        strInvertida[a]=strOriginal[i];
+        a++;
+    }
+    strInvertida[a]='\0';
+}
 
 int main(){
     char palavra[100],palavrainvertida[100];
-    printf("Palavra: ");
-    scanf(" %s",*palavra);
-    printf("!!%s",*palavra);
-
+    while(1){ 
+        printf("Palavra: ");
+        scanf(" %s",palavra);
+        inverte(palavra,palavrainvertida);
+        printf("Resultado: ");
+        if(strcmp(palavra,palavrainvertida)==0){
+            printf("%s é capicua.\n",palavra);
+        }
+        else{
+            printf("%s não é capicua.\n",palavra);
+        }
+    }
     return 0;
 }
